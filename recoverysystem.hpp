bf7b555811611452b4894b373562dfa81486f6a6
Rosalie <144969642+rhughes65@users.noreply.github.com>
2025-10-28 17:07:20 -0600

    Implement RecoverySystem class for parachute deployment

---
+++ recoverysystem.hpp
@@ -0,0 +1,19 @@
+class RecoverySystem {
+public:
+    static void deployParachute();
+    static bool parachuteDeployed();
+};
+
+#include "HardwareIO.hpp"
+
+void RecoverySystem::deployParachute() {
+    // Ensure safe conditions
+    if (!parachuteDeployed()) {
+        HardwareIO::firePyro("PARACHUTE");
+        Telemetry::sendAlert("Parachute deployed", 0);
+    }
+}
+
+bool RecoverySystem::parachuteDeployed() {
+    return HardwareIO::readPyroStatus("PARACHUTE");
+}
