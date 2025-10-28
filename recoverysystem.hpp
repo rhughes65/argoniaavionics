42337debb4256e374121e2b5b385a3b90e4f7900
Rosalie <144969642+rhughes65@users.noreply.github.com>
2025-10-28 17:09:06 -0600

    Remove parachute deployment methods from RecoverySystem

--- recoverysystem.hpp
+++ recoverysystem.hpp
@@ -3,17 +3,3 @@ public:
     static void deployParachute();
     static bool parachuteDeployed();
 };
-
-#include "HardwareIO.hpp"
-
-void RecoverySystem::deployParachute() {
-    // Ensure safe conditions
-    if (!parachuteDeployed()) {
-        HardwareIO::firePyro("PARACHUTE");
-        Telemetry::sendAlert("Parachute deployed", 0);
-    }
-}
-
-bool RecoverySystem::parachuteDeployed() {
-    return HardwareIO::readPyroStatus("PARACHUTE");
-}
